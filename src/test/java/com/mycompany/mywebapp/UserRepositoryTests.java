3f00ff2b58ef8b50cef09f920001cc1c36dc0ffb
praveen-codeprism <praveen.pochammala@codeprism.in>
2022-03-26 21:06:27 +0530

    Initial commit

---
+++ src/test/java/com/mycompany/mywebapp/UserRepositoryTests.java
@@ -0,0 +1,43 @@
+package com.mycompany.mywebapp;
+
+import com.mycompany.mywebapp.user.User;
+import com.mycompany.mywebapp.user.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+    @Autowired private UserRepository repo;
+    @Test
+    public  void testAddNew(){
+        User user=new User();
+        user.setEmail("praveenp447@gmail.com");
+        user.setPassword("praveenp447");
+        user.setFirstname("Praveen");
+        user.setLastname("Pochammala");
+        User savedUser= repo.save(user);
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+
+    }
+    @Test
+    public void TestListAll(){
+        Iterable<User> users = repo.findAll();
+        Assertions.assertThat(users).hasSizeGreaterThan(0);
+        for (User user : users) {
+            System.out.println(user);
+            
+        }
+
+    }
+    @Test
+    public void testUpdate(){
+
+    }
+}
